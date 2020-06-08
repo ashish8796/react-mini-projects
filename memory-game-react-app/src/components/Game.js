2767a0afd435403f2b53e255ce2630ddc2debf41
ashish8796 <ashishsaini.ak@gmail.com>
2020-06-08 20:48:14 +0530

    memory react app completed

--- memory-game-react-app/src/components/Game.js
+++ memory-game-react-app/src/components/Game.js
@@ -99,7 +99,7 @@ function GameLevels(props) {
   return (
     <>
       {
-        true && (
+        gameCompleted && (
           <div className="game-finished">
             <div className="game-completed animate__animated animate__zoomInDown">
               <h1><em>Congratulation!! </em> You Completed the game with {moves} moves in {seconds} seconds. </h1>
